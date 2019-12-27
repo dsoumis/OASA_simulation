cca7c4a83385b4d39320d2a66e1c2f13a98be10a
losetelo <jimsoumis@gmail.com>
2019-12-27 18:13:15 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ΟΑΣΑ</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+<?php
+    include 'Navbar.html'
+?>
+
+</body>
+</html>
